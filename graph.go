d33eda7fc3446a17b21f9a5b6d4d4a96716b8811
Holden Wight <htwight@gmail.com>
2022-06-30 19:41:48 -0600

    Implement thread-safety

--- graph.go
+++ graph.go
@@ -1,8 +1,11 @@
 package htgraph
 
+import "sync"
+
 //Implemented as an adjacency list
 type Graph[T comparable] struct {
 	nodes map[*Node[T]]([]Edge[T])
+	mu    sync.Mutex
 }
 
 type Edge[T any] struct {
@@ -22,11 +25,15 @@ func (g *Graph[T]) NewGraph() Graph[T] {
 }
 
 func (g *Graph[T]) AddNode(n *Node[T]) {
+	g.mu.Lock()
 	g.nodes[n] = []Edge[T]{}
+	g.mu.Unlock()
 }
 
 func (g *Graph[T]) RemoveNode(n *Node[T]) {
+	g.mu.Lock()
 	delete(g.nodes, n)
+	g.mu.Unlock()
 }
 
 func (g *Graph[T]) AddEdge(source *Node[T], target *Node[T]) {
@@ -44,10 +51,14 @@ func (g *Graph[T]) AddWeightedEdge(source *Node[T], target *Node[T], weight floa
 
 	edge := Edge[T]{source: source, target: target, weight: weight}
 
+	g.mu.Lock()
 	g.nodes[source] = append(g.nodes[source], edge)
+	g.mu.Unlock()
 }
 
 func (g *Graph[T]) RemoveEdge(source *Node[T], target *Node[T]) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
 	edges := g.nodes[source]
 	for i, node := range edges {
 		if node.target == target {
@@ -58,6 +69,8 @@ func (g *Graph[T]) RemoveEdge(source *Node[T], target *Node[T]) {
 }
 
 func (g *Graph[T]) HasEdge(source *Node[T], target *Node[T]) bool {
+	g.mu.Lock()
+	defer g.mu.Unlock()
 	edges := g.nodes[source]
 	for _, edge := range edges {
 		if edge.target == target {
@@ -68,6 +81,8 @@ func (g *Graph[T]) HasEdge(source *Node[T], target *Node[T]) bool {
 }
 
 func (g *Graph[T]) Contains(n *Node[T]) bool {
+	g.mu.Lock()
 	_, ok := g.nodes[n]
+	g.mu.Unlock()
 	return ok
 }
